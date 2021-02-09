77357ac400f52330c6022bfa1c8addbf2816d1df
Pranav Ambre <pranavambre123@gmail.com>
2021-02-10 00:24:11 +1100

    commit

---
+++ src/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import styled from 'styled-components';
+const GridWrapper = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  margin-top: 1em;
+  margin-left: 270px;
+  margin-right: 6em;
+  grid-template-columns: repeat(12, 1fr);
+  grid-auto-rows: minmax(25px, auto);
+`; 
+export const About = () => (
+  <GridWrapper>
+    <h2>About Page</h2>
+    <p>This page is not implemented yet</p>
+    
+  </GridWrapper>
+)
